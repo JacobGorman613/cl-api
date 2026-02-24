e514ba5c7ba6a9b8640bd4b57334ff13e2de5c1e
janetzhang321 <15350144+janetzhang321@users.noreply.github.com>
2026-02-23 22:08:30 -0500

    change paths to use env var

--- idpcache.py
+++ idpcache.py
@@ -1,8 +1,23 @@
 import json
 import os.path
+# install pyyaml
+import yaml
+
+def env_constructor(loader, node):
+    value = loader.construct_scalar(node)
+    return os.getenv(value, f"DEFAULT_{value}") # Optional default value
+
+# TODO: Make this run when the file gets imported
+if __name__ == "__main__":
+	yaml.SafeLoader.add_constructor('!env', env_constructor)
+	with open('env.yaml', 'r') as f:
+		config = yaml.safe_load(f)
+		idp_cache_file = os.path.join(config["IDP_CACHE_DIR"], "idp_cache.json")
+		print(idp_cache_file)
+
 # set up cache for idp
 def set_up_cache():
-	if os.path.isfile("idp_cache.json"):
+	if os.path.isfile(idp_cache_file):
 		return False
 	else:
 		cache = {
@@ -10,14 +25,14 @@ def set_up_cache():
         'ng2_datas' : {},
         'user_ids' : {}
     	}
-		with open("idp_cache.json", 'w') as cache_file:
+		with open(idp_cache_file, 'w') as cache_file:
 			json.dump(cache, cache_file)
 		return True
 		
 #write to the cache
 def write_to_cache(cache):
-	if os.path.isfile("idp_cache.json"):
-		with open("idp_cache.json", 'w') as cache_file:
+	if os.path.isfile(idp_cache_file):
+		with open(idp_cache_file, 'w') as cache_file:
 			json.dump(cache, cache_file)
 		return True
 	else:
@@ -25,8 +40,8 @@ def write_to_cache(cache):
 	
 #access cache
 def get_cache():
-	if os.path.isfile("idp_cache.json"):
-		with open("idp_cache.json") as cache_file:
+	if os.path.isfile(idp_cache_file):
+		with open(idp_cache_file) as cache_file:
 			cache = json.load(cache_file)
 		return cache
 	return False
